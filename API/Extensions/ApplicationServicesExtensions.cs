4f980126b049d91213f262a5805866dfa8b97fee
Alban Hysaj <119055538+banhysaj@users.noreply.github.com>
2024-03-13 15:32:24 +0100

    Commit

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -4,6 +4,7 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
@@ -22,6 +23,16 @@ namespace API.Extensions
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
 
+
+            //In here we are adding Redis to effectively managing the customer basket (Cart)
+            //Redis will make it possible for the cart to be temporarily be saved in the memory of our Server
+            services.AddSingleton<IConnectionMultiplexer>(c =>{
+
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });
+
+            //services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
